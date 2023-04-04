72db5e78a65eab402c8a4c9ad0e113ac589da037
julien-lad <julien.ladreyt@gmail.com>
2023-04-04 09:51:01 +0200

    Quete 2 Bulbasaur

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PokemonCard from './components/PokemonCard'
+
+
 
 function App() {
+  return (
+    <figure>
+      <PokemonCard />
+    </figure>
+  );
+}
+  /*
   const [count, setCount] = useState(0)
 
   return (
@@ -31,5 +41,5 @@ function App() {
     </div>
   )
 }
-
+*/
 export default App
